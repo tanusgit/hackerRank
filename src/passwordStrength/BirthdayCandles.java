4389a4622f2065ab809d4866b089796c305b57a4
tanu <24976766+tanusgit@users.noreply.github.com>
2020-03-25 10:23:33 -0700

    candles

--- src/passwordStrength/BirthdayCandles.java
+++ src/passwordStrength/BirthdayCandles.java
@@ -2,7 +2,7 @@ package passwordStrength;
 
 public class BirthdayCandles {
 	public static void main(String[] args) {
-		int[] ar = { 1, 2, 4, 4 };
+		int[] ar = { 18, 20, 20, 20 ,90, 90, 13, 90, 75, 90, 8, 90, 43 };
 		int res = birthdayCakeCandles(ar);
 		System.out.println(res);
 	}
@@ -15,7 +15,29 @@ public class BirthdayCandles {
 			if (ar[i] > max) {
 				max = ar[i];
 			}
+		}
+		for (int i = 0; i < ar.length; i++) {
 			if (max == ar[i]) {
+				count++;
+			}
+		}
+
+		return count;
+
+	}
+	
+	
+	
+	static int birthdayCakeCandles2(int[] ar) {
+		int max = 0;
+		int count = 0;
+		for (int i = 0; i < ar.length; i++) {
+			int prevmax = max;
+			if (ar[i] > max) {
+				max = ar[i];
+				count = 1;
+			}
+			if(prevmax == ar[i]) {
 				count++;
 			}
 		}
